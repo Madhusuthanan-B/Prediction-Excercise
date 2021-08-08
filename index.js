5b629c194358628e690ea311a856a0ce9c5fd072
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-08 14:39:05 +0530

    Plot data in scatterplot with tfjs-vis

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 async function loadData() {
     const houseSalesDataSet = tf.data.csv('http://127.0.0.1:5500/kc_house_data.csv');
-    const top10Data = houseSalesDataSet.take(10);
-    const dataArray = await top10Data.toArray();
-    console.log(dataArray);
+    // const top10Data = houseSalesDataSet.take(10);
+    const points = houseSalesDataSet.map(record => ({ x: record.sqft_living, y: record.price }));
+    // const dataArray = await points.toArray();
+    // console.log(dataArray);
+    plot(await points.toArray(), 'Square Feet')
+
+}
+
+async function plot(pointsArray, featureName) {
+    const surface = { name: `${featureName} vs House Price`, tab: 'Charts' };
+    const series = ['original'];
+    const data = { values: [pointsArray], series };
+    const labels = { xLabel: featureName, yLabel: 'Price' };
+    tfvis.render.scatterplot(surface, data, labels);
 }
 
 loadData();
