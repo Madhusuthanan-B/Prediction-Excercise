6a42c13c31c02c422d055a1424a7a96318043aaa
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-08 15:04:59 +0530

    Denormalization

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ async function loadData() {
     const featureValues = await points.map(p => p.x).toArray();
     const featureTensor = tf.tensor2d(featureValues, [featureValues.length, 1]);
 
+    featureTensor.print();
+
     // Labels (outputs)
     const labelValues = await points.map(p => p.y).toArray();
     const labelTensor = tf.tensor2d(labelValues, [labelValues.length, 1]);
 
     const normalizedFeatureTensor = normalize(featureTensor);
     const normalizedLabelTensor = normalize(labelTensor);
-    
+
     // After normalization, the orignal values will be always between 0 and 1
-    normalizedFeatureTensor.print();
-    normalizedLabelTensor.print();
+    normalizedFeatureTensor.tensor.print();
+    normalizedLabelTensor.tensor.print();
+
+    const denormalized = deNormalize(normalizedFeatureTensor.tensor, normalizedFeatureTensor.min, normalizedFeatureTensor.max)
+    denormalized.print();
 
 }
 
@@ -31,13 +36,24 @@ async function plot(pointsArray, featureName) {
 
 function normalize(tensor) {
     /*
-    * Formula for min-max normilization
+    * Formula for min-max normqlization
     * x` = (x-min(x))/(max(x)-min(x))
     */
     const min = tensor.min();
     const max = tensor.max();
-    const normalizedTensor = tensor.sub(min).div(max.sub(min));
-    return normalizedTensor;
+    const range = max.sub(min);
+    const normalizedTensor = tensor.sub(min).div(range);
+    return {
+        tensor: normalizedTensor,
+        min,
+        max
+    };
+}
+
+function deNormalize(tensor, min, max) {
+    const range = max.sub(min);
+    const deNormalizedTensor = tensor.mul(range).add(min);
+    return deNormalizedTensor;
 }
 
 loadData();
