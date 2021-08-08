2b6e898b4de62ad7b5752c4fd30e60d45a7b8320
Madhusuthanan-B <madhub081011@gmail.com>
2021-08-08 14:56:18 +0530

    Normalization using min-max normalization

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 async function loadData() {
     const houseSalesDataSet = tf.data.csv('http://127.0.0.1:5500/kc_house_data.csv');
-    // const top10Data = houseSalesDataSet.take(10);
     const points = houseSalesDataSet.map(record => ({ x: record.sqft_living, y: record.price }));
-    // const dataArray = await points.toArray();
-    // console.log(dataArray);
-    plot(await points.toArray(), 'Square Feet')
+    plot(await points.toArray(), 'Square Feet');
+
+    // Features (inputs)
+    const featureValues = await points.map(p => p.x).toArray();
+    const featureTensor = tf.tensor2d(featureValues, [featureValues.length, 1]);
+
+    // Labels (outputs)
+    const labelValues = await points.map(p => p.y).toArray();
+    const labelTensor = tf.tensor2d(labelValues, [labelValues.length, 1]);
+
+    const normalizedFeatureTensor = normalize(featureTensor);
+    const normalizedLabelTensor = normalize(labelTensor);
+    
+    // After normalization, the orignal values will be always between 0 and 1
+    normalizedFeatureTensor.print();
+    normalizedLabelTensor.print();
 
 }
 
@@ -14,6 +26,18 @@ async function plot(pointsArray, featureName) {
     const data = { values: [pointsArray], series };
     const labels = { xLabel: featureName, yLabel: 'Price' };
     tfvis.render.scatterplot(surface, data, labels);
+}
+
+
+function normalize(tensor) {
+    /*
+    * Formula for min-max normilization
+    * x` = (x-min(x))/(max(x)-min(x))
+    */
+    const min = tensor.min();
+    const max = tensor.max();
+    const normalizedTensor = tensor.sub(min).div(max.sub(min));
+    return normalizedTensor;
 }
 
 loadData();
